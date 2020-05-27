0e3bcd3a975567d0985f0fbdc5fae5784a70b69d
Kayla <kaylamyhome@gmail.com>
2020-05-27 17:09:54 -0400

    Fix bug with active class being added to icon instead of link. Include landing image

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,17 +15,18 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
   var element = document.getElementsByClassName("menu")[0];
-
   element.addEventListener("click", toggleActiveSection);
+
   function toggleActiveSection(tab) {
       var elems = document.querySelector(".active");
       if (elems != null) {
         elems.classList.remove("active");
       }
-      tab.target.classList.add("active");
-      console.log("change active status");
-      // console.log(elems);
-      console.log(tab.target);
-
+      if (tab.target.nodeName === 'I') {
+        tab.target.parentNode.classList.add("active");
+      }
+      else {
+        tab.target.classList.add("active");
+      }
 	}
 });
