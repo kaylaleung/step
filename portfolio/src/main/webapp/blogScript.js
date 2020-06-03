6ec896b88f1284a283de965060a5fd8608bb3892
Kayla <kaylamyhome@gmail.com>
2020-06-03 14:26:10 -0400

    Remove redundant js code

--- portfolio/src/main/webapp/blogScript.js
+++ portfolio/src/main/webapp/blogScript.js
@@ -1,24 +1,3 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-
-    let element = document.getElementsByClassName("menu")[0];
-    element.addEventListener("click", (tab) => {
-        toggleActiveSection(tab);
-    });
-});
-
-function toggleActiveSection(tab) {
-      let elems = document.querySelector(".active");
-      if (elems != null) {
-        elems.classList.remove("active");
-      }
-      if (tab.target.nodeName === 'I') {
-        tab.target.parentNode.classList.add("active");
-      }
-      else {
-        tab.target.classList.add("active");
-      }
-}
-
 function getComments() {
     fetch('/data').then(response => response.json()).then((comments) => {
         
