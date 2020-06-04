53ff069b29bff8b17fac5e7a30b558421483f260
Kayla <kaylamyhome@gmail.com>
2020-06-03 23:43:44 -0400

    Add multiple blog posts

--- portfolio/src/main/webapp/blogScript.js
+++ portfolio/src/main/webapp/blogScript.js
@@ -1,3 +1,23 @@
+function getPost() {
+  fetch('/post').then(response => response.json()).then((posts) => {
+
+      // console.log(location.search);
+      var number = parent.document.URL.substring(parent.document.URL.indexOf('postNum='), parent.document.URL.length);
+      console.log(number.substring(8))
+
+      const postElement = document.getElementById('post-container');
+
+      console.log(postElement);
+      postElement.innerHTML = '';
+
+      posts.forEach((post) => {
+        console.log(post.title);
+        postElement.innerHTML = post.blogpost;        
+      }) 
+      
+  });
+}
+
 function getComments() {
     fetch('/data').then(response => response.json()).then((comments) => {
         
