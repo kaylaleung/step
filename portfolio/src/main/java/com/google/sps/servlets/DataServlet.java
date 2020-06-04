c99284a7cab02b00839daa348e699798d2a7a735
Kayla <kaylamyhome@gmail.com>
2020-06-04 19:18:41 -0400

    Fix constants and line spacing

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -31,45 +31,41 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-  private static final String textParameter = "text-input";
-  private static final String nameParameter = "name";
-  private static final String timestampParameter = "timestamp";
+  private static final String TEXT_PARAM = "text";
+  private static final String TEXTIN_PARAM = "text-input";
+  private static final String NAME_PARAM = "name";
+  private static final String TIME_PARAM = "timestamp";
+  private static final String COMMENT = "Comment";
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     
-    ArrayList<String> comments;
-
-    Query query = new Query("Comment").addSort("timestampParameter", SortDirection.DESCENDING);
+    Query query = new Query(COMMENT).addSort(TIME_PARAM, SortDirection.DESCENDING);
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
-
-    comments = new ArrayList<String>();
+    ArrayList<String> comments = new ArrayList<String>();
 
     for (Entity entity : results.asIterable()) {
-      String text = (String) entity.getProperty("textParameter");
+      String text = (String) entity.getProperty(TEXT_PARAM);
       comments.add(text);
     }
 
     String json = convertToJson(comments);
-
     response.setContentType("application/json;");
     response.getWriter().println(json);
   }
 
   @Override
-
-  
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     
-    String text = request.getParameter(textParameter);
-    String name = request.getParameter(nameParameter);
+    final String text = request.getParameter(TEXTIN_PARAM);
+    final String name = request.getParameter(NAME_PARAM);
     long timestamp = System.currentTimeMillis();
 
-    Entity taskEntity = new Entity("Comment");
-    taskEntity.setProperty(textParameter, text);
-    taskEntity.setProperty(timestampParameter, timestamp);
-    taskEntity.setProperty(nameParameter, name);
+    Entity taskEntity = new Entity(COMMENT);
+    taskEntity.setProperty(TEXT_PARAM, text);
+    taskEntity.setProperty(TIME_PARAM, timestamp);
+    taskEntity.setProperty(NAME_PARAM, name);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(taskEntity);
