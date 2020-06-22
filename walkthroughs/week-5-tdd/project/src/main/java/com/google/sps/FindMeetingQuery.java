64b6c5fa02c2fbea99a073aff090ffc7075f9e42
Kayla <kaylamyhome@gmail.com>
2020-06-22 14:53:26 -0400

    Remove unnecessary returns

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -36,7 +36,7 @@ public final class FindMeetingQuery {
 
     /* If there are only optional attendees requested, default set all optional attendees to be 
        included */
-    boolean optionalIncluded = request.getAttendees().isEmpty();
+    boolean optionalIncluded = requestAtten.isEmpty();
     
     for (Event event : events) { 
       /* Check if there are attendees in the event that are also requested attendees */
@@ -60,7 +60,7 @@ public final class FindMeetingQuery {
 
       if (timeConflictFound) {
         /* If time conflict found, add available time block up until the start of the event */
-        availableTimes = checkConflict(availableTimes, request, blockStart, eventStart);
+        checkConflict(availableTimes, request, blockStart, eventStart);
         /* If available start time is before the end of the conflict time block, 
            set new available time to after the conflict event is finished. */
         if (blockStart < eventEnd) {
@@ -69,21 +69,21 @@ public final class FindMeetingQuery {
       }
     }
     /* After all events considered, add the entire rest of the day as also available */
-    availableTimes = checkEndOfDay(availableTimes, blockStart);
+    checkEndOfDay(availableTimes, blockStart);
     /* If there are no available times returned, but there are non 
        optional requested attendees, check again for non optional 
        attendee avaliability */
     if (availableTimes.isEmpty() && !requestAtten.isEmpty()) {
-      availableTimes = queryRequiredOnly(events, request);
+      queryRequiredOnly(events, request);
     }
     return availableTimes;
   }
 
-  private Collection<TimeRange> queryRequiredOnly(Collection<Event> events, MeetingRequest request) {
+  private void queryRequiredOnly(Collection<Event> events, MeetingRequest request) {
     Collection<TimeRange> availableTimes = new ArrayList<>();
     /* If the requested time span is longer than a whole day, return no available meeting times */
     if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
-      return availableTimes;
+      return;
     }
     int blockStart = TimeRange.START_OF_DAY;
     Collection<String> requestAtten = request.getAttendees();
@@ -95,7 +95,7 @@ public final class FindMeetingQuery {
       boolean timeConflictFound = !Collections.disjoint(eventAtten, requestAtten);
       /* If time conflict found, add available time block up until the start of the event */
       if (timeConflictFound) {
-        availableTimes = checkConflict(availableTimes, request, blockStart, eventStart);
+        checkConflict(availableTimes, request, blockStart, eventStart);
         /* If available start time is before the end of the conflict time block, 
            set new available time to after the conflict event is finished. */
         if (blockStart < eventEnd) {
@@ -103,25 +103,22 @@ public final class FindMeetingQuery {
         }
       }
     }
-    availableTimes = checkEndOfDay(availableTimes, blockStart);
-    return availableTimes;
+    checkEndOfDay(availableTimes, blockStart);
   }
 
-  private Collection<TimeRange> checkEndOfDay(Collection<TimeRange> availableTimes, int blockStart) {
+  private void checkEndOfDay(Collection<TimeRange> availableTimes, int blockStart) {
     /* After all events have been considered, add time block from last event till end of day */
     if (blockStart < TimeRange.END_OF_DAY) {
       availableTimes.add(TimeRange.fromStartEnd(blockStart, TimeRange.END_OF_DAY, true));
     }
-    return availableTimes;
   }
   
-  private Collection<TimeRange> checkConflict(Collection<TimeRange> availableTimes,  MeetingRequest request, int blockStart, int eventStart) {
+  private void checkConflict(Collection<TimeRange> availableTimes,  MeetingRequest request, int blockStart, int eventStart) {
     int blockEnd = eventStart;
     /* If there is no conflict, and the block meets the duration requirements, add to 
        availableTimes */
     if (blockEnd - blockStart >= request.getDuration()) {
       availableTimes.add(TimeRange.fromStartEnd(blockStart, blockEnd, false));
     }
-    return availableTimes;
   }
 }
